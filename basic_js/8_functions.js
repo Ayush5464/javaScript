6650ef292952be21df0ebc6e2a6862dc87ec1d83
ayush gupta <ag298744@gmail.com>
2025-05-30 12:14:37 +0530

    objects

---
+++ basic_js/8_functions.js
@@ -0,0 +1,22 @@
+// function myname(){
+//     console.log("a");
+//     console.log("y");
+//     console.log("u");
+//     console.log("s");
+//     console.log("h");
+
+// }
+
+// myname()
+
+function addition(a, b) {
+  console.log(a + b);
+}
+// addition(1, "a");
+
+// functions with objects
+
+function calculatePrice(...num1) {
+  return num1;
+}
+console.log(calculatePrice(200, 100, 200));
